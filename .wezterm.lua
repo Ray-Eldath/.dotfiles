99dbd237f181e1e7ff1ef65b007913a1156a3196
Ray-Eldath <ray.eldath@outlook.com>
2025-11-11 23:50:54 +0800

    add MoveTabRelative shortcuts and set SSHMUX domain in launch_menu

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,7 +3,7 @@ local act = wezterm.action
 local config = {}
 
 config.default_prog = { '/opt/homebrew/bin/fish' }
-config.default_domain = 'SSHMUX:tdx0'
+-- config.default_domain = 'SSHMUX:tdx0'
 config.font = wezterm.font 'Cascadia Mono PL'
 config.font_size = 16.0
 config.color_scheme = 'Humanoid light (base16)'
@@ -15,15 +15,27 @@ config.native_macos_fullscreen_mode = true
 config.keys = {
   { key = 'LeftArrow', mods = 'SHIFT', action = act.ActivateTabRelative(-1) },
   { key = 'RightArrow', mods = 'SHIFT', action = act.ActivateTabRelative(1) },
+  { key = 'LeftArrow', mods = 'OPT|SHIFT', action = act.MoveTabRelative(-1) },
+  { key = 'RightArrow', mods = 'OPT|SHIFT', action = act.MoveTabRelative(1) },
   { key = '%', mods = 'SUPER|SHIFT', action = act.SplitVertical },
   { key = '"', mods = 'SUPER|SHIFT', action = act.SplitHorizontal },
   { key = 'w', mods = 'CMD', action = act.CloseCurrentPane { confirm = true } },
+  { key = 'D', mods = 'CMD|SHIFT', action = act.DetachDomain 'CurrentPaneDomain' },
   { key = 't', mods = 'CMD|SHIFT', action = act.ShowLauncher },
   { key = 'LeftArrow', mods = 'SUPER|SHIFT', action = act.ActivatePaneDirection 'Left' },
   { key = 'RightArrow', mods = 'SUPER|SHIFT', action = act.ActivatePaneDirection 'Right' },
   { key = 'UpArrow', mods = 'SUPER|SHIFT', action = act.ActivatePaneDirection 'Up' },
   { key = 'DownArrow', mods = 'SUPER|SHIFT', action = act.ActivatePaneDirection 'Down' },
 }
+local launch_menu = {}
+for _, domain in ipairs({'tdx0', 'midifarm', 'h100'}) do
+  local str = 'SSHMUX:' .. domain
+  table.insert(launch_menu, {
+      label = 'domain `' .. str .. '`',
+      domain = { DomainName = str}
+  })
+end
+config.launch_menu = launch_menu
 
 local TAG_BG_COLOR = '#F7F5F2'
 config.colors = {
